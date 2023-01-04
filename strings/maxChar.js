5fffaee81fb8ef81f0e2bc41c85ec56ccd2deff8
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-03 20:49:10 -0700

    implement maxChar func

--- strings/maxChar.js
+++ strings/maxChar.js
@@ -5,4 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let highestKey;
+    let highestVal = -Infinity;
+
+    for (let char of str) {
+        if (char in charMap) {
+            charMap[char] += 1;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for (let key in charMap) {
+        if (charMap[key] > highestVal) {
+            highestKey = key;
+            highestVal = charMap[highestKey];
+        }
+    }
+
+    return highestKey;
+}
