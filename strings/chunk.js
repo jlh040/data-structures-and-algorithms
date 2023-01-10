1ffdfd8dc76c87ac8b651ac9a9472454c77f2367
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-09 18:47:46 -0700

    fully implement first attempt at chunk function

--- strings/chunk.js
+++ strings/chunk.js
@@ -8,12 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// Linear time compexity O(N) where N is array.length
 function chunk(array, size) {
     const newArr = [];
     let subArrNum = 0;
     let subArrIdx = 0;
 
     for (let i = 0; i < array.length; i++) {
+        if (subArrIdx === 0) {
+            newArr.push([]);
+        }
 
+        newArr[subArrNum][subArrIdx] = array[i];
+        subArrIdx++;
+
+        if (subArrIdx === size) {
+            subArrIdx = 0;
+            subArrNum++;
+        }
     }
+
+    return newArr;
 }
