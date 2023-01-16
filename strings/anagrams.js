6673f2ca3c314ad6f6a8e5d75c996e7b6df0e9ec
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-15 21:28:49 -0700

    create auxiliary character map function

--- strings/anagrams.js
+++ strings/anagrams.js
@@ -1,4 +1,4 @@
-/ --- Directions
+// --- Directions
 // Check to see if two provided strings are anagrams of eachother.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
@@ -8,4 +8,20 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function createCharMap(str) {
+    const charMap = {};
+
+    for (let char of str) {
+        if (!charMap[char]) {
+            charMap[char] = 1;
+        } else {
+            charMap[char] += 1;
+        }
+    }
+}
+
+function anagrams(stringA, stringB) {
+    const strACharMap = createCharMap(stringA);
+    const strBCharMap = createCharMap(stringB);
+
+}
