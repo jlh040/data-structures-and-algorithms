257df0abd2656473e197348666f3a3d50d3cb730
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-23 17:16:07 -0700

    add steps problem

---
+++ strings/steps.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {
+    // create empty string
+    let emptyString = '';
+
+    for (let i = 1; i <= n; i++) {
+        emptyString += '#'.repeat(i);
+        emptyString += ' '.repeat(n - i);
+
+        console.log(emptyString);
+        emptyString = '';
+    }
+}
