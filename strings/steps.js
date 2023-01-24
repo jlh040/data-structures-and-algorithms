14c479dd6a845a21f085ca62fa119467cd6be07a
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-23 17:36:10 -0700

    add another implementation for steps problem

--- strings/steps.js
+++ strings/steps.js
@@ -28,3 +28,19 @@ function steps(n) {
         emptyString = '';
     }
 }
+
+function steps2(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+
+        for (let col = 0; col < n; col++) {
+            if (col <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log(stair);
+    }
+}
