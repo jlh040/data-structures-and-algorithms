481bc88e2ebf9960fe45ae1330ee3ce6e2e20e90
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-31 15:18:56 -0700

    add iterative solution to fibonacci problem

--- math/fibonacci.js
+++ math/fibonacci.js
@@ -19,5 +19,17 @@ function fibRecursive(n) {
 }
 
 function fibIterative(n) {
+    const result = [];
 
+    for (let i = 0; i <= n; i++) {
+        if (i === 0) {
+            result.push(0)
+        } else if (i === 1) {
+            result.push(1)
+        } else {
+            result.push(result[i - 1] + result[i - 2])
+        }
+    }
+
+    return result[n];
 }
