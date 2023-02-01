b517b4416aa9232e1361be8e193569e9083a95ea
Jeff Hall <jeff.leon.hall@gmail.com>
2023-01-31 20:01:30 -0700

    make base case of fibonacci recursion even shorter

--- math/fibonacci.js
+++ math/fibonacci.js
@@ -9,10 +9,8 @@
 //   fib(4) === 3
 
 function fibRecursive(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if (n < 2) {
+        return n;
     } else {
         return fib(n - 1) + fib(n - 2);
     }
