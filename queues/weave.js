786b27193fde4c0d1d2ff0f7f333c12fd4f662c4
Jeff Hall <jeff.leon.hall@gmail.com>
2023-02-08 21:15:41 -0700

    implement slightly cleaner weave function

--- queues/weave.js
+++ queues/weave.js
@@ -40,3 +40,19 @@ function weave(sourceOne, sourceTwo) {
 
     return weavedQueue;
 }
+
+function weave2(sourceOne, sourceTwo) {
+    const q = new Queue();
+
+    while (sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+
+        if (sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+
+    return q;
+}
