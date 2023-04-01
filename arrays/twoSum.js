5954078721349f936c19f2773969b2e94ed55b35
Jeff Hall <jeff.leon.hall@gmail.com>
2023-04-01 11:36:40 -0700

    implement Two Sum problem

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -8,3 +8,22 @@
 // return true since 1 + 21 = 22
 // Example:
 //   twoSum([0, 1, 2, 3, 5, 8, 13, 21, 34], 22) --> true
+
+function twoSum(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum === target) {
+            return true;
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return false;
+}
