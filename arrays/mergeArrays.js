6bbcfc10c21a64c4765db6febe7009ae6eba21d8
Jeff Hall <jeff.leon.hall@gmail.com>
2023-04-01 13:19:13 -0700

    add description for Merge Arrays problem

---
+++ arrays/mergeArrays.js
@@ -0,0 +1,8 @@
+// --- Directions
+// Given two sorted arrays of integers, return a single array
+// that combines both of them and is also sorted
+// For example, for the arrays
+//  [0, 1, 2, 5] and [4, 7, 8]
+// the function should return [0, 1, 2, 4, 5, 7, 8]
+// Example:
+//   mergeArrays([5, 7, 9], [1, 3, 6]) --> [1, 3, 5, 6, 7, 9]
