6b594ab3b2b0d89603d9ca39dbffdfe2cb0eccce
Jeff Hall <jeff.leon.hall@gmail.com>
2023-04-01 13:26:35 -0700

    implement Merge Array function

--- arrays/mergeArrays.js
+++ arrays/mergeArrays.js
@@ -6,3 +6,29 @@
 // the function should return [0, 1, 2, 4, 5, 7, 8]
 // Example:
 //   mergeArrays([5, 7, 9], [1, 3, 6]) --> [1, 3, 5, 6, 7, 9]
+
+function mergeArrays(arr1, arr2) {
+    const mergedArr = [];
+    let i = 0;
+    let j = 0;
+    
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArr.push(arr1[i]);
+            i++
+        } else {
+            mergedArr.push(arr2[j]);
+            j++
+        }
+    }
+
+    while (i < arr1.length) {
+        mergedArr.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        mergedArr.push(arr2[j]);
+        j++;
+    }
+}
