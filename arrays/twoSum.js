3f4bcee88382fec45e313d2388dd0843e2c2efe9
Jeff Hall <jeff.leon.hall@gmail.com>
2023-04-01 11:33:16 -0700

    add description for Two Sum problem

---
+++ arrays/twoSum.js
@@ -0,0 +1,10 @@
+// --- Directions
+// Given a sorted array of integers, and a sum: target,
+// return true if there exists a pair within the array
+// that sums to target, and false otherwise.
+// For example, for the array
+//  [0, 1, 2, 3, 5, 8, 13, 21, 34]
+// and the target = 22,
+// return true since 1 + 21 = 22
+// Example:
+//   twoSum([0, 1, 2, 3, 5, 8, 13, 21, 34], 22) --> true
