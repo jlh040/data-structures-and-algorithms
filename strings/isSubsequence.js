ef0c225fdd0dc5aa14cc00711e926acb0ba56471
Jeff Hall <jeff.leon.hall@gmail.com>
2023-04-01 19:58:27 -0700

    fix minor error and clean up isSubsequence problem

--- strings/isSubsequence.js
+++ strings/isSubsequence.js
@@ -12,18 +12,12 @@ function isSubsequence(s, t) {
     let i = 0;
     let j = 0;
 
-    while (j < t.length) {
-        if (s[i] === j[t]) {
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) {
             i++;
-            j++;
-        } else {
-            j++
         }
+        j++;
     }
 
-    if (i === s.length) {
-        return true;
-    }
-
-    return false;
+    return i === s.length;
 }
